3f1d21f45710ec09ac2be0b79aef8cd417522be0
Keigo Yamamoto <keigo.yamamoto@rakus-partners.co.jp>
2022-09-28 10:06:37 +0900

    first commit

---
+++ src/main/java/com/example/demo/HouseHoldaccountsBook1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseHoldaccountsBook1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseHoldaccountsBook1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/domain/Account.java
@@ -0,0 +1,62 @@
+package com.example.domain;
+
+public class Account {
+
+	int id;
+	private String date;
+	private int type;
+	private String item;
+	private int price;
+
+	public Account() {
+
+	}
+
+	public Account(String date, int type, String item, int price) {
+		this.date = date;
+		this.type = type;
+		this.item = item;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
