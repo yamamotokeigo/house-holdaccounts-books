cbdece601f3650c4d90314aca00e25a076ee215b
Keigo Yamamoto <keigo.yamamoto@rakus-partners.co.jp>
2022-10-06 13:21:07 +0900

    家計簿登録の完了

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -2,12 +2,14 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.domain.Account;
+import com.example.demo.form.AccountForm;
 import com.example.demo.service.HouseHoldAccountsService;
 
 @Controller
@@ -19,6 +21,7 @@ public class AccountController {
 	@Autowired
 	public AccountController(HouseHoldAccountsService service) {
 		this.service = service;
+	
 	}
 
 	// 全件表示を行う
@@ -34,7 +37,16 @@ public class AccountController {
 	
 	//新規登録
 	@RequestMapping("/insert")
-	public String insert(Account account) {
+	public String insert(Model model) {
+		
 		return "account/insert";
 	}
+	
+	@RequestMapping("/insertFinish")
+    public String insert(Model model, AccountForm form) {
+		Account account = new Account();
+		BeanUtils.copyProperties(form, account);
+		service.insert(account);
+        return "account/InsertFinish";
+    }
 }
